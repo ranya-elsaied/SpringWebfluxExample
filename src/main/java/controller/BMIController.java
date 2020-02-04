f4a3b6d87fac3c5bfd707f0e7cd0b1e1f151e89b
ranya-elsaied <Has_890_Good>
2020-02-04 13:01:01 +0200

    Add new end point to add persons

--- src/main/java/controller/BMIController.java
+++ src/main/java/controller/BMIController.java
@@ -1,11 +1,13 @@
 package controller;
 
+import model.Person;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import repository.PersonRepository;
 import service.BMIService;
 
 import java.time.Duration;
@@ -19,7 +21,10 @@ public class BMIController {
     @Autowired
     private BMIService bmiService;
 
-    @GetMapping
+    @Autowired
+    private PersonRepository repository;
+
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Double> getBMI(){
         return bmiService.calculateBMI();
     }
@@ -27,6 +32,11 @@ public class BMIController {
     @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Long> getEvents(){
         return Flux.interval(Duration.ofSeconds(1));
+    }
+
+    @PostMapping("/person")
+    Mono<Void> create(@RequestBody Person person) {
+        return this.repository.save(person).then();
     }
 
 }
